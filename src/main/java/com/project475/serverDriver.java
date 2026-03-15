19d4f1614bc92777ba1f2ef01d22bc99067e4e78
GitThemGitThem <gs53@uw.edu>
2026-03-14 21:29:27 -0700

    added my front end into the driverfile

--- src/main/java/com/project475/serverDriver.java
+++ src/main/java/com/project475/serverDriver.java
@@ -31,6 +31,10 @@ public class serverDriver {
             System.out.println("4. List All Courses Assignments");
             System.out.println("5. List All Courses for a Student");
             System.out.println("6. Create A Teacher");
+            System.out.println("7. Create Grade Category");
+            System.out.println("8. Update Grade Category");
+            System.out.println("9. Delete Grade Category");
+            System.out.println("10. Update an Assignment Grade for all");
             System.out.println("Insert a number to select one of the options above:");
 
             userInput = sc.nextInt();
@@ -58,6 +62,14 @@ public class serverDriver {
                 case 6:
                     client.createTeacher_Client(sc);
                     break;
+                case 7:
+                    client.createGradeCategory(sc);
+                case 8:
+                    client.updateGradeCategory(sc);
+                case 9:
+                    client.deleteGradeCategory(sc);
+                case 10:
+                    client.updateAssignmentGradeForAll(sc);
                 default:
                     System.out.println("Invalid input");
             }
